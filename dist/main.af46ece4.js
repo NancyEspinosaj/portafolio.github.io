f82008285db6003b659364298bc2fa217a87ba21
Nancy Esipnosa <nancyespinosaj@gmail.com>
2021-06-03 01:45:50 -0500

    Creacion de la segunda y tercera parte

--- dist/main.af46ece4.js
+++ dist/main.af46ece4.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./assets\\images\\foto.png":[["foto.c5e87154.png","assets/images/foto.png"],"assets/images/foto.png"],"_css_loader":"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64945" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57079" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
